b442f7ad3f9b7c73d15c7456a1606a4523a8582e
Juan Carlo Valencia <carlo.valencia066@gmail.com>
2018-01-04 12:37:41 +0800

    Commit with errors

--- application/controllers/AdminDashboard.php
+++ application/controllers/AdminDashboard.php
@@ -6,20 +6,40 @@ class AdminDashboard extends CI_Controller {
         //---> MODELS HERE!
         
         //---> LIBRARIES HERE!
-
         
         //---> SESSIONS HERE!
-        
+        if ($this->session->has_userdata('isloggedin') == FALSE) {
+            //user is not yet logged in
+            $this->session->set_userdata("err_4", "Login First!");
+            redirect(base_url().'login/');
+        }else{
+            $current_user = $this->session->userdata("current_user");
+            if($this->session->userdata("user_access") == "user"){
+                //USER!
+                $this->session->set_flashdata("err_5", "You are currently logged in as ".$current_user->user_firstname." ".$current_user->user_lastname);
+                redirect(base_url()."UserDashboard");
+            }
+            else if($this->session->userdata("user_access") == "subadmin"){
+                //SUBADMIN!
+                $this->session->set_flashdata("err_5", "You are currently logged in as ".$current_user->user_firstname." ".$current_user->user_lastname);
+                redirect(base_url()."SubadminDashboard");
+            }
+            else if($this->session->userdata("user_access") == "admin"){
+                //ADMIN!
+                //Do nothing!
+            }
+        }
     }
     
     public function index(){
+        $current_user = $this->ManageUsers_model->get_users("admin", array("admin_id" => $this->session->userdata("userid")))[0];
         $data = array(
             'title' => "Dashboard",
             'trails' => $this->AuditTrail_model->get_audit_trail("event", "user", "event.user_id = user.user_id", array("event_classification" => "trail")),
             'logs' => $this->UserLogs_model->get_userlogs("event", "user", "event.user_id = user.user_id", array("event_classification" => "log")),
-            //FOR DUMMY VARIABLES
-            'user_name' => "Juan Carlo D.R. Valencia",
-            'user_picture' => "images/user/jc.png",
+            //NAV INFO
+            'user_name' => $current_user->admin_firstname." ".$current_user->admin_lastname,
+            'user_picture' => $current_user->admin_picture,
             'user_access' => "Administrator"
         );
         $this->load->view("dashboard/includes/header", $data);
