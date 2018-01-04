7c1ad8fcf9497f1dc1f94c574dc7494b5d25dc03
abzaguirre <32188141+abzaguirre@users.noreply.github.com>
2018-01-04 12:37:34 +0800

    Register

--- application/views/register/includes/header.php
+++ application/views/register/includes/header.php
@@ -9,28 +9,18 @@
 
         <!-- Favicons -->
         <link rel="shortcut icon" href="<?= $this->config->base_url() ?>images/logo/icon.png">
-        <link href="<?= base_url() ?>assets/main/img/apple-touch-icon.png" rel="apple-touch-icon">
-
         <!-- Google Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
 
         <!-- Bootstrap CSS File -->
         <link href="<?= base_url() ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="<?= base_url() ?>assets/bootstrap-datepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
 
         <!-- Libraries CSS Files -->
         <link href="<?= base_url() ?>assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-        <link href="<?= base_url() ?>assets/animate/animate.min.css" rel="stylesheet">
-
-        <!-- Main Stylesheet File -->
-        <link href="<?= base_url() ?>assets/main/css/style.css" rel="stylesheet">
-
-        <!-- =======================================================
-          Theme Name: Regna
-          Theme URL: https://bootstrapmade.com/regna-bootstrap-onepage-template/
-          Author: BootstrapMade.com
-          License: https://bootstrapmade.com/license/
-        ======================================================= -->
+        
     </head>
+
     <style>
         body{
             background-color:#f0c514;
