4fb0bcd2339dac07e21dcce27694c7b5911c7ea2
abzaguirre <32188141+abzaguirre@users.noreply.github.com>
2018-01-07 01:46:15 +0800

    PetAdoption

---
+++ application/models/PetAdoption_model.php
@@ -0,0 +1,15 @@
+<?php
+
+class PetAdoption_model extends CI_Model {
+
+    public function fetchPetDesc($table, $where = NULL) {
+
+        if (!empty($where)) {
+            $this->db->where($where);
+        }
+        $this->db->order_by('pet_added_at', 'desc');
+        $query = $this->db->get($table);
+        return ($query->num_rows() > 0 ) ? $query->result() : FALSE;
+    }
+
+}
