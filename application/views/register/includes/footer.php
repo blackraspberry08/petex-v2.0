7f13e839c9d248b11e02a2f789788f0e47b07dab
Juan Carlo Valencia <carlo.valencia066@gmail.com>
2018-02-17 19:01:49 +0800

    Registration Done Except Email

--- application/views/register/includes/footer.php
+++ application/views/register/includes/footer.php
@@ -1,6 +1,42 @@
-<!-- Bootstrap core JavaScript-->
-<script src="<?= base_url() ?>assets/jquery/jquery.min.js"></script>
-<script src="<?= base_url() ?>assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- GOOGLE MAPS-->
+<script src="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyD1VJrwMKjjvTi626jR6pdoaNdIHKEdp0c"></script>
+<script>
+
+    //EMPTY ADDRESS INPUT
+    var map;
+    function initialize() {
+        // Set up the map
+        var mapOptions = {
+            zoom: 18,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            streetViewControl: false,
+        };
+        map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+        google.maps.event.addDomListener(window, 'load', initialize);
+    }
+</script>
+
+<!-- GEO COMPLETE -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/geocomplete/1.7.0/jquery.geocomplete.min.js"></script>
+<script>
+
+    $(function () {
+        $("#geocomplete").geocomplete({
+            map: "#google-map",
+            details: "form",
+            country: 'ph',
+        }).bind("geocode:result", function (event, result) {
+//            console.log("===== FOOTER =====");
+//            console.log(event);
+//            console.log(result);
+        });
+    });
+</script>
+
+
+<!-- BOOTSTRAP -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.bundle.min.js" integrity="sha384-VspmFJ2uqRrKr3en+IG0cIq1Cl/v/PHneDw6SQZYgrcr8ZZmZoQ3zhuGfMnSR/F2" crossorigin="anonymous"></script>
+<!-- DATETIME PICKER -->
 <script src="<?= base_url() ?>assets/bootstrap-datepicker/js/bootstrap-datetimepicker.js"></script>
 <script type="text/javascript">
     var dt = new Date();
@@ -16,5 +52,9 @@
     });
 
 </script>  
+<!-- SweetAlert -->
+<script src = "https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+
+
 </body>
 </html>
--- application/views/register/includes/header.php
+++ application/views/register/includes/header.php
@@ -13,12 +13,17 @@
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
 
         <!-- Bootstrap CSS File -->
-        <link href="<?= base_url() ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
         <link href="<?= base_url() ?>assets/bootstrap-datepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
+        <!-- SweetAlert -->
+        <link rel = "stylesheet" href ="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
+   
+        <!-- JQUERY -->
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 
-        <!-- Libraries CSS Files -->
-        <link href="<?= base_url() ?>assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
         
+        <!-- Libraries CSS Files -->
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     </head>
 
     <style>
@@ -27,3 +32,13 @@
         }
     </style>
     <body>
+        <script>
+            document.addEventListener("DOMContentLoaded", function () {
+                $('.preloader-background').delay(800).fadeOut('slow');
+                $('.preloader-wrapper').delay(800).fadeOut();
+                $('[data-toggle="tooltip"]').tooltip({
+                    container: 'body'
+                 });
+            });
+        </script>
+        <?php include 'preloader.php' ?>
