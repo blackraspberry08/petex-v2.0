7f7fd6ed3fa0eee85e98c2129322f9824768bc53
abzaguirre <32188141+abzaguirre@users.noreply.github.com>
2018-03-04 17:11:01 +0800

    Session

--- application/views/main/main.php
+++ application/views/main/main.php
@@ -409,7 +409,7 @@
                                                             <div class="col-md-3"></div>
                                                             <div class="col-md-3"> 
                                                                 <a href="<?= base_url() ?>download/adoption_application_form.pdf" onclick="window.open('<?= base_url() ?>PetAdoption/download_exec/<?= $pet->pet_id ?>');
-                                                                        return true;" class = 'btn btn-outline-primary' download> Download <i class = "fa fa-download"></i></a >
+                                                                                    return true;" class = 'btn btn-outline-primary' download> Download <i class = "fa fa-download"></i></a >
                                                             </div>
                                                             <div class="col-md-3">
                                                                 <a href="<?= base_url() ?>PetAdoption/petAdoptionOnlineForm_exec/<?= $pet->pet_id ?>" class="btn btn-outline-primary">Fill up the Form Online</a>
