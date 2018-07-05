10e72ff82b04140fcf21ff6ed5fb7a04d93ca357
abzaguirre <32188141+abzaguirre@users.noreply.github.com>
2018-07-05 14:10:12 +0800

    Excel

---
+++ application/libraries/IOFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("PHPExcel/IOFactory.php");
+
+class IOFactory extends PHPExcel_IOFactory {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
+?>
